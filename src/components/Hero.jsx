1c2964041f042b371fefdfdef75d308e269ff55f
Baibhavos <baibhavkumar303@gmail.com>
2024-08-04 23:00:31 +0530

    Hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { logo } from "../assets";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    return (
+        <header className="w-full justify-center items-center flex-col">
+            <nav className="flex justify-between items-center w-full mb-10 pt-3">
+                <img src={logo} alt="app-logo" className="w-28 object-contain" />
 
-export default Hero
+                <button
+                    type="button"
+                    onClick={() => window.open("https://github.com")}
+                    className="black-btn"
+                >
+                    GitHub
+                </button>
+            </nav>
+
+            <h1 className="head_text">
+                Summarize articles with <br />
+                <span className="orange_gradient">OpenAI GPT-4</span>
+            </h1>
+            <h2 className="desc">
+                Simplify your reading with Summarizer, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+            </h2>
+        </header>
+    );
+};
+
+export default Hero;
